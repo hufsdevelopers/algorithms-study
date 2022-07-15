e0386dff7be7a7a5df0b84c1d3f5239b220cf19b
Jeonhui <48307153+Jeonhui@users.noreply.github.com>
2022-07-15 15:36:02 +0900

    solved

--- Jeonhui/problem/4-3.py
+++ Jeonhui/problem/4-3.py
@@ -3,18 +3,15 @@ import sys
 
 def move(x, y, d):
     global N, M, Map
-    if not (0 <= y < N and 0 <= x < M):
+    if not (0 <= y < N and 0 <= x < M) or Map[y][x] == 1:
         return 0
-    if Map[y][x] == 1:
-        return 1
     Map[y][x] = 1
-
     m = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-    a = move(x+m[(d+1)%4][0], y+m[(d+1)%4][1], (d + 1) % 4)
-    b = move(x+m[(d+2)%4][0], y+m[(d+2)%4][1], (d + 2) % 4)
-    c =move(x+m[(d+3)%4][0], y+m[(d+3)%4][1], (d + 3) % 4)
-
-    return a + b + c
+    a = move(x + m[d][0], y + m[d][1], d)
+    b = move(x + m[(d + 1) % 4][0], y + m[(d + 1) % 4][1], (d + 1) % 4)
+    c = move(x + m[(d + 2) % 4][0], y + m[(d + 2) % 4][1], (d + 2) % 4)
+    d = move(x + m[(d + 3) % 4][0], y + m[(d + 3) % 4][1], (d + 3) % 4)
+    return a + b + c + d +  1
 
 
 N, M = map(int, sys.stdin.readline().split())
